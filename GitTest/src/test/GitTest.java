c4709de91d6d9a493ad616d983a46be9c93baf23
Stanislav Zhelev <stanislav.zhelev@abv.bg>
2015-02-25 23:59:28 +0200

    update

--- GitTest/src/test/GitTest.java
+++ GitTest/src/test/GitTest.java
@@ -4,7 +4,9 @@ public class GitTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("aaaaaaaaaaaaaa");
+		System.out.println("bbbbbbbbbbbbbbbbbb");
+		System.out.println("cccccccccccccccc");
 	}
 
 }
